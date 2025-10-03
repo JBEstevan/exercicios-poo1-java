e4c7631d6e114ae0ec872fef723aa2b39df6198c
Juan Estevan <127903274+JBEstevan@users.noreply.github.com>
2025-10-02 23:59:14 -0300

    Update ContaBancariaTeste.java
    fix; Testar melhor

--- src/main/java/aula09/ContaBancariaTeste.java
+++ src/main/java/aula09/ContaBancariaTeste.java
@@ -10,10 +10,10 @@ public class ContaBancariaTeste {
 		
 		contaBancaria1.setTitular("João");
 		
-		System.out.println("Numero da Conta: " + contaBancaria1.getNumero());
-		System.out.println("Nome do Titular: " + contaBancaria1.getTitular());
+		//System.out.println("Numero da Conta: " + contaBancaria1.getNumero());
+		//System.out.println("Nome do Titular: " + contaBancaria1.getTitular());
 		
-		System.out.println("===============================");
+		//System.out.println("===============================");
 		
 		System.out.println(contaBancaria1);
 	}
