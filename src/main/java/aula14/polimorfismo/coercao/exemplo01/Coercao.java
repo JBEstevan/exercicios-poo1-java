efed3e97ae8d27c41303c08bcbb3a63624cdf8b1
JBEstevan <jbe.estevan@gmail.com>
2025-11-17 23:19:55 -0300

    feat: Adicionar exemplo de Polimorfismo (coerção)

---
+++ src/main/java/aula14/polimorfismo/coercao/exemplo01/Coercao.java
@@ -0,0 +1,13 @@
+package aula14.polimorfismo.coercao.exemplo01;
+
+public class Coercao {
+	public static void main(String[] args) {
+
+		int x = 15;
+		double y = x;
+
+		int z = (int) y;
+
+		System.out.println(y);
+	}
+}
